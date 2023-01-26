51d5bc6945ca66d37a5c230d1148f078db4debbe
IKHARO Paul <paulevansikharo@gmail.com>
2023-01-26 11:30:28 +0100

    refactor: Added gh link to footer

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,7 +3,7 @@ import './styles/Footer.css'
 export const Footer = () => {
     return (
         <footer className='footer-section'>
-            <p>Designed and Built by Paulev</p>
+            <p>Designed and Built by <a href='https://github.com/PEvansik'>Paul</a></p>
         </footer>
     )
 }
